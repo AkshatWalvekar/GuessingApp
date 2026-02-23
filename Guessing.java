0d55b805a421b56cbb983d2abc7e946d1a749614
AkshatWalvekar <akshatwalvekar@gmail.com>
2026-02-23 12:06:30 +0530

    Coded UC1 for Game Initialization

--- Guessing.java
+++ Guessing.java
@@ -1,36 +1,5 @@
 import java.util.Random;
-class GameConfig{
-	
-	private final int MIN = 1;
-	private final int MAX = 100;
-	private final int MAX_ATTEMPTS = 7;
-	private final int MAX_HINTS = 3;
-	
-	int targetNumber;
-	
-	public GameConfig(){
-		Random random = new Random();
-		this.targetNumber = random.nextInt(MAX - MIN + 1) + MIN;
-	}
-	
-	public int getTargetNumber(){
-		return targetNumber; 
-	}
-	
-	public int getMaxAttempts(){
-		return MAX_ATTEMPTS;
-	}
-	
-	public int getMaxHints(){
-		return MAX_HINTS;
-	}
-	
-	public void showRules(){
-		System.out.println("Guess a number between " + MIN + " and " + MAX);
-		System.out.println("You have " + MAX_ATTEMPTS + " attempts.");
-		System.out.println("Hints will be provided after wrong guesses.\n");
-	}
-}
+
 
 public class Guessing {
 	public static void main(String[] args)
@@ -38,5 +7,6 @@ public class Guessing {
 		System.out.println("Welcome to the Guessing App");
 		GameConfig gameConfig = new GameConfig();
 		gameConfig.showRules();
+		
 	}
 }
